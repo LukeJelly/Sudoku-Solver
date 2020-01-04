7e2a1d14ce691a0bde9ab443cad8b17b54513bc8
Luke Kelly <48305827+LukeJelly@users.noreply.github.com>
2020-01-03 18:05:34 -0800

    initial commit

---
+++ src/SudokuSolver.java
@@ -0,0 +1,179 @@
+import java.util.Stack;
+
+/**
+ * A simple class that solves a Sudoky.
+ * @author Luke Kelly
+ * @version 1.0
+ */
+class SudokuSolver {
+    /**
+     * Solves the given Sudoku, preconditions: 
+     * 1. Empty space is reperesnted by 0s. 
+     * 2. The sudoku is not already invalid(No duplicates in a row, colum, or 3x3 box,except zeros). 
+     * 3. The sudoku is a normal 9x9 sudoku.
+     * @return a solved sudoku is the sudoku is solvable, null if not.
+     */
+    public static  int[][] solveSudoku(int[][] original) {
+        //Clone the original so I don't change the original array
+        int[][] workingSudoku = cloneSudoku(original);
+        //Find all the emtpy indexes in the array, this simplifes the algorithm.
+        Stack<Integer[]> allEmtpyIndexes = findAllEmptySpaces(workingSudoku);
+        //Solve it, if they method returns false the sudoku is unsolvable.
+        if(solveSudoku(workingSudoku, allEmtpyIndexes)){
+            return workingSudoku;
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * Private recursive helper for solveSudoku
+     * @param workingSudoku the Sudoku we are trying to solve
+     * @param allEmptyIndexes a Stack holding all the emtpy locations
+     * @return true if the sudoku is solved, false if not.
+     */
+    private static boolean solveSudoku(int[][] workingSudoku, 
+                                       Stack<Integer[]> allEmptyIndexes) {
+        /* Algorithm Explanation:
+            1. Grab the first index that is empty from the stack,
+            2. Grab all information from that index.
+            3. Loop through all the numbers from 1-9 and see if they fit work in the empty space.
+                i.  If the number is allowed, put it in that index, and restart the loop from the next index.
+                    a. If the recursive call returns true then we know that number works in that location return true;
+                    b. If the recursive call retruns false then we know that number does not work,
+                       place a EMPTY_CELL back in that location and continue back at step 3.
+                ii. If none of the numbers work, we put this index back in the stack, and return false
+            4. If at top of stack and return false then we know all possible permutations were tried return false
+               else we return true because we have a solved puzzle.
+         */
+        int EMPTY_CELL = 0; //Place holder for an Emtpy cell
+        while (!allEmptyIndexes.isEmpty()) {
+            //1.
+            Integer[] workingIndex = allEmptyIndexes.pop();
+            //2.
+            int row = workingIndex[0];
+            int col = workingIndex[1];
+            //3.
+            for (int number = 1; number <= 9; number++) {
+                if (isAllowed(workingSudoku, row, col, number)) {
+                    //3 i.
+                    workingSudoku[row][col] = number;
+                    if (solveSudoku(workingSudoku, allEmptyIndexes)){
+                        //3 i a.
+                        return true;
+                    } else {
+                        //3 i b.
+                        workingSudoku[row][col] = EMPTY_CELL;
+                    }
+                }
+            
+            }
+            //3 ii.
+            allEmptyIndexes.add(workingIndex);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if that number is already in the row, colum, 3x3 box that our
+     * index is in.
+     * @param workingSudoku the Sudoku we are working in
+     * @param rowIndex the index of the row we are working in
+     * @param colIndex the index of the colum we are working in
+     * @param number the number we are trying to place there
+     * @return true if the number works, false if it doesn't.
+     */
+    private static boolean isAllowed(int[][] workingSudoku, int rowIndex, int colIndex, int number) {
+        return !(containsInRow(workingSudoku, rowIndex, number) ||
+                 containsInCol(workingSudoku, colIndex, number) || 
+                 containsInBox(workingSudoku, rowIndex, colIndex, number));
+    }
+
+    /**
+     * Checks if the given number exists in this row.
+     * @param sudoku the Sudoku we are working in.
+     * @param rowIndex the index of the row.
+     * @param number The number we are checking against.
+     * @return true if the number is in there, false if not.
+     */
+    private static boolean containsInRow(int[][] sudoku, int rowIndex, int number) {
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[rowIndex][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the given number exists in this colum.
+     * @param sudoku the Sudoku we are working in.
+     * @param colIndex the index of the colum.
+     * @param number The number we are checking against.
+     * @return true if the number is in there, false if not.
+     */
+    private static boolean containsInCol(int[][] sudoku, int colIndex, int number) {
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[i][colIndex] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the given number exists in the box.
+     * @param sudoku the Sudoku we are working in.
+     * @param row the index of the row.
+     * @param col the index of the colum.
+     * @param number The number we are checking against.
+     * @return true if the number is in there, false if not.
+     */
+    private static boolean containsInBox(int[][] sudoku, int row, int col, int number) {
+        int r = row - row % 3;
+        int c = col - col % 3;
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                if (sudoku[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Finds all the emtpy indexes in the Sudoku.
+     * @param sudoku the sudoku we are searching through.
+     * @return A stack with all the empty indexes in the Sudoku.
+     */
+    private static Stack<Integer[]> findAllEmptySpaces(int[][] sudoku) {
+        Stack<Integer[]> allIndexes = new Stack<>();
+        int size = sudoku.length - 1;
+        for (int outer = size; outer >= 0; outer--) {
+            for (int inner = size; inner >= 0; inner--) {
+                if (sudoku[outer][inner] == 0) {
+                    allIndexes.push(new Integer[] { outer, inner, 1 });
+                }
+            }
+
+        }
+        return allIndexes;
+    }
+
+    /**
+     * Clones the given sudoku, so we can make changes with out affecting the 
+     * original.
+     * @param original the original Sudoku
+     * @return a copy of the original.
+     */
+    private static int[][] cloneSudoku(int[][] original) {
+        int sizeSudoku = original.length;
+        int[][] newArr = new int[sizeSudoku][sizeSudoku];
+        for (int i = 0; i < sizeSudoku; i++) {
+            newArr[i] = original[i].clone();
+        }
+        return newArr;
+    }
+}
---
+++ tests/TestSolver.java
@@ -0,0 +1,190 @@
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Simple test clas to test my Solve Sudoku.
+ * 
+ * @author Luke Kelly
+ * @version 1.0
+ */
+public class TestSolver {
+    // The size of a sudoku
+    private int DIMENSIONS_OF_SUDOKU = 9;
+    // Interger representation of an emtpy space
+    private int EMPTY_SPACE = 0;
+    // The original sudoku generated every time.
+    int[][] sudoku = new int[DIMENSIONS_OF_SUDOKU][DIMENSIONS_OF_SUDOKU];
+
+    /*
+     * Generates a random sudoku from the python file I found online.
+     */
+    @Before
+    public void generateRandomSudoku() {
+        // Python command to tell terminal
+        String command = "python ";
+        // Location of the file.
+        String pathToFile = "\"C:\\Users\\LukeJ\\OneDrive\\Documents\\"
+                + "Programming\\Java\\Sudoku Solver\\src\\Generator.py\"";
+
+        // The process that will run to create the file.
+        Process generator;
+        try {
+            // Run python script to generate a sudoku file.
+            generator = Runtime.getRuntime().exec(command + pathToFile);
+            /*
+             * Wait for file to be created, the python process will close itself 
+             * when it is done.
+             */
+            generator.waitFor();
+        } catch (IOException e) {
+            /*
+             * Used in the testCreateFile test to make sure the generator is 
+             * working properly
+             */
+            fail("Could not run the python script");
+        } catch (InterruptedException e) {
+            /*
+             * Used in the testCreateFile test to make sure the generator is 
+             * working properly
+             */
+            fail("The process was interrupted");
+        }
+
+        // File location of the file
+        File puzzle = new File(
+                "C:\\Users\\LukeJ\\OneDrive\\Documents\\" + 
+                "Programming\\Java\\Sudoku Solver\\puzzle.txt");
+
+        try {
+            // Scanner to read through the file.
+            Scanner inFile = new Scanner(puzzle);
+            int outer = 0;
+            while (inFile.hasNextLine()) {
+                String line = inFile.nextLine();
+                for (int i = 0; i < DIMENSIONS_OF_SUDOKU; i++) {
+                    // Stores the int value of the char in the array.
+                    sudoku[outer][i] = (line.charAt(i) - '0');
+                }
+                outer++;
+            }
+            inFile.close();
+        } catch (FileNotFoundException e) {
+            /*
+             * Used in the testCreateFile test to make sure the generator is 
+             * working properly
+             */
+            fail("Puzzle file was not created.");
+        }
+    }
+
+    @Test
+    /**
+     * Test to make sure the file generator is working properly.
+     */
+    public void testCreateFile() {
+        generateRandomSudoku();
+    }
+
+    @Test
+    public void testSolver() {
+        // Check 10 different Sudokus.
+        int numTests = 10;
+        for (int testNumber = 0; testNumber < numTests; testNumber++) {
+            generateRandomSudoku();
+            int[][] solvedSudoku = SudokuSolver.solveSudoku(this.sudoku);
+            if (!checkSameSudoku(solvedSudoku))
+                fail("The sudoku was not the same as the original " + testNumber);
+            if (!isComplete(solvedSudoku)) {
+                for (int[] integers : solvedSudoku) {
+                    System.out.println(Arrays.toString(integers));
+                }
+                fail("The sudoku was not correct " + testNumber);
+            }
+        }
+    }
+
+    /**
+     * Checks if the given sudoku is complete and that there are no duplicate
+     * nubmers.
+     */
+    private boolean isComplete(int[][] compSudoku) {
+        // Check if all rows are correct first.
+        for (int outer = 0; outer < DIMENSIONS_OF_SUDOKU; outer++) {
+            // Add one to DIMENSIONS_OF_SUDOKU because the number 9 is allowed.
+            boolean[] hasBeenUsed = new boolean[DIMENSIONS_OF_SUDOKU + 1];
+            Arrays.fill(hasBeenUsed, false);
+            for (int inner = 0; inner < DIMENSIONS_OF_SUDOKU; inner++) {
+                int value = compSudoku[outer][inner];
+                boolean check = checkValue(hasBeenUsed, value);
+                if (!check)
+                    return false;
+            }
+        }
+
+        // Check if all colums are correct second.
+        for (int outer = 0; outer < DIMENSIONS_OF_SUDOKU; outer++) {
+            // Add one to DIMENSIONS_OF_SUDOKU because the number 9 is allowed.
+            boolean[] hasBeenUsed = new boolean[DIMENSIONS_OF_SUDOKU + 1];
+            Arrays.fill(hasBeenUsed, false);
+            for (int inner = 0; inner < DIMENSIONS_OF_SUDOKU; inner++) {
+                int value = compSudoku[inner][outer];
+                boolean check = checkValue(hasBeenUsed, value);
+                if (!check)
+                    return false;
+            }
+        }
+        /* If we get here then we know all colums and rows are correct */
+        return true;
+    }
+
+
+    private boolean checkValue(boolean[] hasBeenUsed, int value) {
+        boolean returnedValue = true;
+        //If the value has already been used.
+        if (hasBeenUsed[value] == true) {
+            returnedValue = false;
+        /*If the value is less than max number allowed
+          If the value is less than one
+          If the value is less is an Emtpy space.
+          return false.*/
+        
+        } else if (value > DIMENSIONS_OF_SUDOKU || value < 1 || value == EMPTY_SPACE) {
+            returnedValue = false;
+        } else {
+            //Allowed number store it so it can't be used again.
+            hasBeenUsed[value] = true;
+            //ReturnedValue is already set to true so don't change it.
+        }
+        return returnedValue;
+    }
+
+    /**
+     * Checks if the given sudoku is the same as the original. 
+     */
+    private boolean checkSameSudoku(int[][] compSudoku) {
+        //Loop through every index.
+        for (int outer = 0; outer < DIMENSIONS_OF_SUDOKU; outer++) {
+            for (int inner = 0; inner < DIMENSIONS_OF_SUDOKU; inner++) {
+                /*
+                 * If there isn't and empty space in the original at this index
+                 * check this location agains the completed Sudoku.
+                 */
+                if (sudoku[outer][inner] != EMPTY_SPACE) {
+                    if (sudoku[outer][inner] != compSudoku[outer][inner])
+                    //If they are not the same return false.
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
